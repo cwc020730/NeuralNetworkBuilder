1228b5292b45753b49514c6e0d6067bd8c27bbab
cwc020730 <xxzhangjinghexx@gmail.com>
2024-05-07 19:17:18 -0400

    Split areas for different purposes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import "./App.css";
 
 function App() {
     return (
-        <div className='App'>
+        <div className="app">
+            <header className="header">Header</header>
+            <aside className="sidebar">Sidebar</aside>
+            <main className="main">Main Content</main>
+            <footer className="footer">Footer</footer>
         </div>
     );
 }
