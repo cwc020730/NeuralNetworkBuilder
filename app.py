ec82f417340322e1f6ce55a7d8de0554e24e91e9
cwc020730 <xxzhangjinghexx@gmail.com>
2024-05-12 18:18:32 -0400

    Minor update to the style

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,12 @@ CORS(app)
 @app.route('/api/message')
 def send_message():
     return jsonify({'message': 'Hello from Flask!'})
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    print(data)
+    return jsonify({'status': 'Data received!'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
