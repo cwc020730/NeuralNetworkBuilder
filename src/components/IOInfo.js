9262dc9568358ad47f3ef4260440dc5b128f2b57
cwc020730 <xxzhangjinghexx@gmail.com>
2024-05-15 19:04:21 -0400

    Add a file for IOInfo

---
+++ src/components/IOInfo.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { AppContext } from './AppContext';
+import { idToUnitMap } from './D3Canvas';
+import unitList from './UnitList.json';
+
+const IOInfo = () => {
+}
+
+export default IOInfo;
