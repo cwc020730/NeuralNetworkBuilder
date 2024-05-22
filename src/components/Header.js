145b1ab2a0e90e7bcebfef00ac7fceee20bb5d11
cwc020730 <xxzhangjinghexx@gmail.com>
2024-05-21 22:19:05 -0400

    Implemented a dropdown for the header-file button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,38 @@
-// Header.js
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 
 const Header = () => {
-  return (
-    <div className='header-container'>
-        <div className='logo'></div>
-        <div className='button-container'>
-            <button className='header-button'>File</button>
-            <button className='header-button'>Edit</button>
-            <button className='header-button'>Help</button>
+    const [isListVisible, setIsListVisible] = useState(false);
+
+    const handleFileButtonClick = () => {
+        setIsListVisible(!isListVisible);
+    };
+
+    return (
+        <div className='header-container'>
+            <div className='logo'></div>
+            <div className='button-container'>
+                <div className='header-dropdown-container'>
+                    <button className='header-button' onClick={handleFileButtonClick}>
+                        File
+                    </button>
+                    {isListVisible && (
+                        <div className='header-dropdown-menu'>
+                            <button className='dropdown-button'>New</button>
+                            <button className='dropdown-button'>Open</button>
+                            <button className='dropdown-button'>Save</button>
+                        </div>
+                    )}
+                </div>
+                <button className='header-button'>Edit</button>
+                <button className='header-button'>Help</button>
+            </div>
+            <div className='header-blank-reserve'></div>
+            <div className='header-action-buttons'>
+                <button className='header-button'>Export</button>
+                <button className='header-button'>Run</button>
+            </div>
         </div>
-        <div className='header-blank-reserve'></div>
-        <div className='header-action-buttons'>
-            <button className='header-button'>Export</button>
-            <button className='header-button'>Run</button>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
