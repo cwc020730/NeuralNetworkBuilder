83faa357d8ab1d4818c8e78f5a03ac530fe2d290
cwc020730 <xxzhangjinghexx@gmail.com>
2024-05-26 08:49:37 -0400

    Add some styles to data-selection-item

--- src/components/DataSelectionController.js
+++ src/components/DataSelectionController.js
@@ -1,57 +1,37 @@
 
-import React from "react";
+import { React, useState } from "react";
 
 const DataSelectionController = () => {
 
+    const [selectedId, setSelectedId] = useState(null);
+
     const dataSelectionList = [
-        {
-            id: 1,
-            name: 'Data Selection 1'
-        },
-        {
-            id: 2,
-            name: 'Data Selection 2'
-        },
-        {
-            id: 3,
-            name: 'Data Selection 3'
-        },
-        {
-            id: 4,
-            name: 'Data Selection 4'
-        },
-        {
-            id: 5,
-            name: 'Data Selection 5'
-        },
-        {
-            id: 6,
-            name: 'Data Selection 6'
-        },
-        {
-            id: 7,
-            name: 'Data Selection 7'
-        },
-        {
-            id: 8,
-            name: 'Data Selection 8'
-        },
-        {
-            id: 9,
-            name: 'Data Selection 9'
-        },
-        {
-            id: 10,
-            name: 'Data Selection 10'
-        }
+        { id: 1, name: 'Data Selection 1' },
+        { id: 2, name: 'Data Selection 2' },
+        { id: 3, name: 'Data Selection 3' },
+        { id: 4, name: 'Data Selection 4' },
+        { id: 5, name: 'Data Selection 5' },
+        { id: 6, name: 'Data Selection 6' },
+        { id: 7, name: 'Data Selection 7' },
+        { id: 8, name: 'Data Selection 8' },
+        { id: 9, name: 'Data Selection 9' },
+        { id: 10, name: 'Data Selection 10' }
     ];
+
+    const handleItemClick = (id) => {
+        setSelectedId(id);
+    };
 
     return (
         <div className="data-selection-controller">
             <div className="data-selection-item-container">
                 {dataSelectionList.map((dataSelection) => {
                     return (
-                        <div key={dataSelection.id} className="data-selection-item">
+                        <div
+                            key={dataSelection.id}
+                            className={`data-selection-item ${selectedId === dataSelection.id ? 'selected' : ''}`}
+                            onClick={() => handleItemClick(dataSelection.id)}
+                        >
                             {dataSelection.name}
                         </div>
                     );
