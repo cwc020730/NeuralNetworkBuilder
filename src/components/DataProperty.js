278d62527eb4666670c4bbce7b656413d12946b0
cwc020730 <xxzhangjinghexx@gmail.com>
2024-05-30 10:12:56 -0400

    Finish styling data property panel

--- src/components/DataProperty.js
+++ src/components/DataProperty.js
@@ -1,24 +1,83 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { AppContext } from "./AppContext";
 
-const DataProperty = ({ data }) => {
+const DataProperty = () => {
     const { selectedUnitId, selectedDataName, unitData } = useContext(AppContext);
+    const [expandedKeys, setExpandedKeys] = useState({});
 
-    // Helper function to render nested objects
-    const renderObject = () => {
-        if (unitData[selectedUnitId] && unitData[selectedUnitId][selectedDataName]) {
-            const data = unitData[selectedUnitId][selectedDataName];
-            return (
-                <div className="data-property">
-                    <pre>{JSON.stringify(data, null, 2)}</pre>
-                </div>
-            );
-        }
+    const toggleExpand = (key) => {
+        setExpandedKeys((prevExpandedKeys) => ({
+            ...prevExpandedKeys,
+            [key]: !prevExpandedKeys[key],
+        }));
+    };
+
+    const renderData = () => {
+        const data = unitData[selectedUnitId][selectedDataName];
+        return (
+            <>
+                {Object.keys(data).map((key) => {
+                    const value = data[key];
+                    const jsonValue = JSON.stringify(value, null, 2);
+                    const isExpanded = expandedKeys[key];
+                    const isLong = jsonValue.length > 30;
+
+                    let sizestr = "";
+                    if (key === "value" && isLong && Array.isArray(value) && data["shape"]) {
+                        let size = 1;
+                        data["shape"].forEach((dim) => {
+                            size *= dim;
+                        });
+                        sizestr = ` (${size} elements)`;
+                    }
+
+                    return (
+                        <div className="data-property-kv-pair" key={key}>
+                            <div className="data-property-kv-pair-k">
+                                {key}:&nbsp;
+                            </div>
+                            <div className="data-property-kv-pair-v">
+                                {isLong ? (
+                                    isExpanded ? (
+                                        <>
+                                            <button 
+                                                className='data-property-kv-pair-v-btn-collapse' 
+                                                onClick={() => toggleExpand(key)}>
+                                            </button>
+                                            <pre>{jsonValue}</pre>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <button 
+                                                className='data-property-kv-pair-v-btn-expand' 
+                                                onClick={() => toggleExpand(key)}>
+                                                {sizestr}
+                                            </button>
+                                        </>
+                                    )
+                                ) : (
+                                    <>{jsonValue}</>
+                                )}
+                            </div>
+                        </div>
+                    );
+                })}
+            </>
+        );
     };
 
     return (
-        <div className="data-property">
-            {unitData ? renderObject() : <p>No unit data available</p>}
+        <div className="data-property-container">
+            <div className="data-property-title">
+                PROPERTIES
+            </div>
+            <div className="data-property">
+                {unitData && unitData[selectedUnitId] && unitData[selectedUnitId][selectedDataName] ? (
+                    renderData()
+                ) : (
+                    <></>
+                )}
+            </div>
         </div>
     );
 };
