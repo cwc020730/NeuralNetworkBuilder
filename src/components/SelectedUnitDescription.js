912c4dcf300e9f4ea4f278a76c5f063d26c51571
cwc020730 <xxzhangjinghexx@gmail.com>
2024-06-14 04:04:05 -0400

    Fix category description on selectedunitdescription panel

--- src/components/SelectedUnitDescription.js
+++ src/components/SelectedUnitDescription.js
@@ -22,10 +22,21 @@ const SelectedUnitDescription = () => {
     const selectedUnit = idToUnitMap.get(selectedUnitId);
     const unitTypeInfo = UnitList[selectedUnit.type];
 
+    const getCategoryString = (category) => {
+        let categoryString = '';
+        for (let i = 0; i < category.length; i++) {
+            categoryString += category[i];
+            if (i < category.length - 1) {
+                categoryString += ' -> ';
+            }
+        }
+        return categoryString;
+    }
+
     return (
         <div className="selected-unit-description">
             <div className='selected-unit-bold-label'>{unitTypeInfo.in_unit_label}</div>
-            <div className='selected-unit-category'>{unitTypeInfo.category}</div>
+            <div className='selected-unit-category'>{getCategoryString(unitTypeInfo.category)}</div>
             <div className='selected-unit-description-text-container'>
                 <div className='selected-unit-uuid'>UUID: {selectedUnitId}</div>
                 <div className='selected-unit-description-text'>{unitTypeInfo.description}</div>
