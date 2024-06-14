9c916ca92faaa7186bed9e4d0f1597fa1cb87bdf
cwc020730 <xxzhangjinghexx@gmail.com>
2024-06-14 03:39:25 -0400

    Improved category menu style

--- src/components/CategoryMenu.js
+++ src/components/CategoryMenu.js
@@ -23,7 +23,9 @@ const CategoryItem = ({ category, subcategories, selectedCategories, handleCateg
       <li className='category-dropdown-menu-list'>
         <div className={selectedCategories.has(category) ? "category-item-selected" : "category-item"}>
           <span onClick={() => handleCategorySelect(category)} onMouseEnter={setOpen} className="category-name">{category}</span>
-          <div onClick={() => handleCategorySelect(category)} onMouseEnter={setOpen}>{">"}</div>
+          <div onClick={() => handleCategorySelect(category)} onMouseEnter={setOpen}>
+            {Object.keys(subcategories).length == 0 ? "": ">"}
+          </div>
         </div>
         {(expandedCategory === category || isChild(category, expandedCategory)) && Object.keys(subcategories).length > 0 && (
           <div className="subcategory">
@@ -46,14 +48,14 @@ const CategoryMenu = ({ categories, handleCategorySelect, selectedCategories, ex
     <ul className='category-dropdown-menu'>
         {Object.keys(categories).map(category => (
             <CategoryItem
-            key={category}
-            category={category}
-            subcategories={categories[category].subcategories}
-            handleCategorySelect={handleCategorySelect}
-            selectedCategories={selectedCategories}
-            expandedCategory={expandedCategory}
-            setExpandedCategory={setExpandedCategory}
-            parentMap={parentMap}
+              key={category}
+              category={category}
+              subcategories={categories[category].subcategories}
+              handleCategorySelect={handleCategorySelect}
+              selectedCategories={selectedCategories}
+              expandedCategory={expandedCategory}
+              setExpandedCategory={setExpandedCategory}
+              parentMap={parentMap}
             />
         ))}
     </ul>
